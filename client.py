a07874de4d1b8f3cbd46cba8e141fa5531c0ea30
ikrokegb <ikrokegb@gmail.com>
2023-08-08 15:04:14 +0300

    slightly improve the GUI and fix join button bug

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ import tkinter as tk
 from tkinter import scrolledtext
 from tkinter import messagebox
 
+
 HOST = '127.0.0.1'
 PORT = 1234
 
@@ -38,6 +39,7 @@ def connect():
     threading.Thread(target=listen_for_messages_from_server, args=(client,)).start()
 
     username_textbox.config(state=tk.DISABLED)
+    username_button.config(state=tk.DISABLED)
 
 def send_message():
     message = message_textbox.get()
@@ -56,31 +58,39 @@ root.grid_rowconfigure(0, weight=1)
 root.grid_rowconfigure(1, weight=4)
 root.grid_rowconfigure(2, weight=1)
 
-top_frame = tk.Frame(root, width=600, height= 100, bg=DARK_GREY)
+top_frame = tk.Frame(root, width=600, height=100, bg=DARK_GREY)
 top_frame.grid(row=0, column=0, sticky=tk.NSEW)
 
-middle_frame = tk.Frame(root, width=600, height= 400, bg=MEDIUM_GREY)
+middle_frame = tk.Frame(root, width=600, height=400, bg=MEDIUM_GREY)
 middle_frame.grid(row=1, column=0, sticky=tk.NSEW)
 
-bottom_frame = tk.Frame(root, width=600, height= 100, bg=DARK_GREY)
+bottom_frame = tk.Frame(root, width=600, height=100, bg=DARK_GREY)
 bottom_frame.grid(row=2, column=0, sticky=tk.NSEW)
 
 username_label = tk.Label(top_frame, text='Enter username: ', font=FONT, bg=DARK_GREY, fg=WHITE)
 username_label.pack(side=tk.LEFT, padx=10)
 
-username_textbox = tk.Entry(top_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=26)
+username_textbox = tk.Entry(top_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=28)
 username_textbox.pack(side=tk.LEFT)
+username_textbox.config(highlightthickness=0, bd=0)
 
-username_button = tk.Button(top_frame, text='Join', font=FONT, bg=DARK_GREY, fg=WHITE, command=connect)
-username_button.pack(side=tk.LEFT, padx=12)
+username_button = tk.Button(top_frame, text='Join', font=FONT, bg=LIGHT_BLUE, fg=WHITE, command=connect)
+username_button.pack(side=tk.LEFT, padx=10)
+username_button.config(highlightthickness=0, bd=0)
 
-message_textbox = tk.Entry(bottom_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=39)
+message_textbox = tk.Entry(bottom_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=41)
 message_textbox.pack(side=tk.LEFT, padx=10)
+message_textbox.config(highlightthickness=0, bd=0)
+
+message_textbox.pack(side=tk.LEFT, padx=10)
+message_textbox.config(highlightthickness=0, bd=0)
 
 message_button = tk.Button(bottom_frame, text='Send', font=FONT, bg=LIGHT_BLUE, fg=WHITE, command=send_message)
-message_button.pack(side=tk.LEFT, padx=15)
+message_button.pack(side=tk.LEFT, padx=2, pady=2)
+message_button.config(highlightthickness=0, bd=0)
 
-message_box = scrolledtext.ScrolledText(middle_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=67, height=23)
+message_box = scrolledtext.ScrolledText(middle_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=50, height=23)
+message_box.config(highlightthickness=0, bd=0)
 message_box.config(state=tk.DISABLED)
 message_box.pack(side=tk.TOP)
 
