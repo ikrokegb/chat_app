50837c02645411b5c23fdeece62850861b5b6aaf
ikrokegb <ikrokegb@gmail.com>
2023-08-19 18:20:46 +0300

    create new server file

--- server.py
+++ server.py
@@ -1,72 +1,54 @@
 import socket
 import threading
 
-HOST = '127.0.0.1'
-PORT = 1234
-LISTENER_LIMIT = 5
-active_clients = [] #connected users
+HOST = '127.0.0.1' #local
+PORT = 12345
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+clients = []
+nicknames = []
 
 
-def listen_for_messages(client, username):
+def broadcast(message):
+    for client in clients:
+        client.send(message)
 
+
+def handle(client):
     while 1:
-
-        message = client.recv(2048).decode('utf-8') # listen messeges
-        if message != '':
-
-            final_message = username + '~' + message #name~mes
-            send_messages_to_all(final_message)
-        else:
-            print(f'The message send from client {username} is empty')
-
-
-def send_message_to_client(client, message): #for one client
-
-    client.sendall(message.encode())  #utf-8 default
-
-
-def send_messages_to_all(message):
-
-    for user in active_clients:
-
-        send_message_to_client(user[1], message)
-
-
-def client_handler(client):
-    while 1:
-
-        username = client.recv(2048).decode('utf-8') # listen username
-        if username != '':
-            active_clients.append((username, client))
-            prompt_message = "SERVER~" + f"{username} added to the chat"
-            send_messages_to_all(prompt_message)
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} left the chat'.encode('ascii'))
+            nicknames.remove(nickname)
             break
-        else:
-            print("Client username is empty")
-
-    threading.Thread(target=listen_for_messages, args=(client, username, )).start()
 
 
-def main():
-
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    #1 - IPv4    2 - TCP
-
-    try:
-        server.bind((HOST, PORT))
-        print(f"Running the server on {HOST} {PORT}")
-    except socket.error as e:
-        print(e)
-        #print(f'Unable to bind to host {HOST} and port {PORT} ')
-
-    server.listen(LISTENER_LIMIT)
-
+def receive():
     while 1:
-
         client, address = server.accept()
-        print(f"Succesfully connected to client {address[0]}, {address[1]}")
+        print(f'Connected with {str(address)}')
 
-        threading.Thread(target=client_handler, args=(client, )).start()
+        client.send('POLLY'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f'Nickname of the client is {nickname}')
+        broadcast(f'{nickname} joined the chat'.encode('ascii'))
+        client.send('Connected to the server'.encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(client, ))
+        thread.start()
 
 
-if __name__ == "__main__":
-    main()
+print('Server is listening...')
+receive()
