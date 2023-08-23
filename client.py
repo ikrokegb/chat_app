98be10fbe26b02c3697caeb628ed1d2b1f1b4383
ikrokegb <ikrokegb@gmail.com>
2023-08-23 14:22:46 +0300

    create password for admin (c)

--- client.py
+++ client.py
@@ -3,17 +3,28 @@ import threading
 
 
 nickname = input('choose a nickname: ')
-
+if nickname == 'admin':
+    password = input("Enter password for admin: ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 12345))
 
+stop_thread = False
+
 def receive():
     while 1:
+        global stop_thread
+        if stop_thread:
+            break
         try:
             message = client.recv(1024).decode('ascii')
             if message == 'NICK':
                 client.send(nickname.encode('ascii'))
+                next_message = client.recv(1024).decode('ascii')
+                if next_message == 'PASS':
+                    if client.send(password.encode('ascii')) == 'REFUSE':
+                        print("Connection was refused! Wrong password.")
+                        stop_thread = True
             else:
                 print(message)
         except:
